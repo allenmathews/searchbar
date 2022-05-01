3782b224e76609b0c78672023ba757ec72d9f4dc
Allen Mathews <allenjoemathews@gmail.com>
2022-05-01 20:25:19 +1000

    final commit

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
 document.getElementById("searchInput").addEventListener("keyup", function(event) {
     let searchQuery = event.target.value.toLowerCase();
     let allNamesDOMCollection = document.getElementsByClassName('name');
-    let (let counter = 0; counter < allNamesDOMCollection.length; counter++) {
+
+    for (let counter = 0; counter < allNamesDOMCollection.length; counter++) {
         const currentName = allNamesDOMCollection[counter].textContent.toLowerCase();
-        console.log(currentName);
+
+        if (currentName.includes(searchQuery)) {
+            allNamesDOMCollection[counter].style.display = "block";
+        } else {
+            allNamesDOMCollection[counter].style.display = "none";
+        }
     }
 });
