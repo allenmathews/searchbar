b7f3e73b9809b181cbaece317ab4fe1ae3a09261
Allen Mathews <allenjoemathews@gmail.com>
2022-05-01 20:15:35 +1000

    fourth commit

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 document.getElementById("searchInput").addEventListener("keyup", function(event) {
     let searchQuery = event.target.value.toLowerCase();
     let allNamesDOMCollection = document.getElementsByClassName('name');
-    console.log(allNamesDOMCollection[1].textContent);
+    let (let counter = 0; counter < allNamesDOMCollection.length; counter++) {
+        const currentName = allNamesDOMCollection[counter].textContent.toLowerCase();
+        console.log(currentName);
+    }
 });
